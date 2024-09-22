60d5c4f45ccfc9d37dc92c5a5aff573f457630a5
Yusuke-Null <yusuke.soccer21108@gmail.com>
2024-09-22 13:39:52 +0900

    test3

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,6 @@ class CommentsController < ApplicationController
     @comment = article.comments.build
   end
 
-  # bbb
   def create
     article = Article.find(params[:article_id])
     @comment = article.comments.build(comment_params)
