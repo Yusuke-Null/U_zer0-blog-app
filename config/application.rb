455dc807b62bc7274619e91836ff9895c7765209
Yusuke-Null <yusuke.soccer21108@gmail.com>
2024-09-24 17:14:56 +0900

    画像保存先の変更

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module UZer0BlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    Bundler.require(*Rails.groups)
+    Dotenv::Railtie.load
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
